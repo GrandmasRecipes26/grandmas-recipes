c12e9877538d4919720537e00b7bf4de3de08a4c
Grandma's Receipes <grandmasrecipes26@gmail.com>
2026-08-01 18:21:02 +0530

    Fixed MySQL environment variable names

--- app.py
+++ app.py
@@ -46,11 +46,11 @@ razorpay_client = razorpay.Client(
 app.config['SESSION_PERMANENT'] = True
 
 
-app.config['MYSQL_HOST'] = os.getenv('MYSQLHOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQLUSER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQLPASSWORD')
-app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT'))
-app.config['MYSQL_DB'] = os.getenv('MYSQLDATABASE')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 # PROFILE IMAGE UPLOAD
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
